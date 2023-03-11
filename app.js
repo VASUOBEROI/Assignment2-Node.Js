e955c7c909030bff20590fabdb1b79669acea45c
Vasu Oberoi <Vasu.Oberoi@ivanti.com>
2023-03-12 00:46:25 +0530

    Middleware-Added-/and/users

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const express=require('express'); // 3rd Party Package available in npm.
 
 const app=express();
 
+
+// This Route Handler is responsible for Handling all the routes that starts with /users/so-on
+app.use('/users',(req,res,next)=>{
+
+res.send('<h1>Hi from Users. Welcome to Users Page.</h1>');
+
+});
+
+// Order of / should be last because otherwise it run for every request
+// This will be responsible for all routes starting with '/'.
+app.use('/',(req,res,next)=>{
+res.send('<h1>Hi from Nothing.Welcome to Main Page.</h1>');
+});
+
+
+
 // Middleware Code:
 app.use((req,res,next)=>{
       
